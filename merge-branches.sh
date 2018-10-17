4e9af57188e58bfb65a3d568ec1290631c331f55
Thanh Truong <hopchuathurac@gmail.com>
2018-10-18 00:59:51 +0200

    Better doing the job in a temp directory

--- merge-branches.sh
+++ merge-branches.sh
@@ -2,6 +2,30 @@
 Merge all branches using Octupus merge mode
 '
 #!/bin/sh
+
+TMPWORKDIR=$(basename $(mktemp -d -p /tmp))
+cd /tmp/$TMPWORKDIR
+
+ADD_SECRET='ssh-add /etc/git-secret/ssh'
+GIT_CLONE='rm -rf scripts; git clone --branch master git@github.com:Thanh-Truong/scripts.git --no-checkout'
+GIT_CHECKOUT_MERGE_SCRIPT='cd scripts; git fetch; git checkout origin merge-branches.sh; cd ..'
+
+# Pulling the Octupus script 'merge-branches.sh'
+ssh-agent sh -c "$ADD_SECRET; $GIT_CLONE"
+ssh-agent sh -c "$ADD_SECRET; $GIT_CHECKOUT_MERGE_SCRIPT"
+
+chmod +x merge-branches.sh
+
+# Clean up
+clean_up(){
+    cd /tmp
+    rm -rf $TMPWORKDIR
+    unset TMPWORKDIR
+    unset ADD_SECRET
+    unset GIT_CLONE
+    unset GIT_CHECKOUT_MERGE_SCRIPT
+}
+
 merge_branches() { 
     LIST_BRANCHES=''
     for branch in `git for-each-ref --sort='-committerdate' --format='%(refname)' refs/remotes/origin`;do
@@ -23,3 +47,5 @@ trap 'abort' 0
 set -e
 merge_branches
 trap : 0
+
+trap cleanup EXIT
