32d18f577edcb8596cb1f709bb4c6e19119a1af8
Thanh Truong <hopchuathurac@gmail.com>
2018-10-18 23:40:28 +0200

    Reduce hard-coded facts

--- octupus-master.sh
+++ octupus-master.sh
@@ -2,6 +2,14 @@
 Merge all branches using Octupus merge mode
 '
 #!/bin/sh
+
+SSH_PRIVATE_KEY=/etc/git-secret/ssh
+OCTUPUS_FOLDER=scripts
+OCTUPUS_GIT=git@github.com:Thanh-Truong/scripts.git
+OCTUPUS_SH=octupus-merge.sh
+REPO_FOLDER=data-airflow-dags
+
+
 make_temp_folder() {
     TMPWORKDIR=$(basename $(mktemp -d -p /tmp))
     cd /tmp/$TMPWORKDIR
@@ -11,7 +19,7 @@ make_temp_folder() {
 setup_git() {
     echo "Setting Git.................."
     eval "$(ssh-agent -s)"
-    ssh-add /etc/git-secret/ssh    
+    ssh-add $SSH_PRIVATE_KEY 
 }
 
 clone_repo() {
@@ -21,26 +29,26 @@ clone_repo() {
 
 pull_octupus() {
     echo "Pulling Octupus ..............."
-    rm -rf scripts
-    clone_repo "git@github.com:Thanh-Truong/scripts.git" "--no-checkout"
-    cd scripts
-    git fetch && git checkout origin octupus-merge.sh
+    rm -rf $OCTUPUS_FOLDER
+    clone_repo "$OCTUPUS_GIT" "--no-checkout"
+    cd $OCTUPUS_FOLDER
+    git fetch && git checkout origin $OCTUPUS_SH
     cd ..
 }
 
 pull_repo() {
     echo "Pulling Repo ....................."
-    rm -rf data-airflow-dags
+    rm -rf $REPO_FOLDER
     clone_repo "git@github.com:TV4/data-airflow-dags.git" ""    
-    mv scripts/octupus-merge.sh data-airflow-dags/octupus-merge.sh
+    mv $OCTUPUS_FOLDER/$OCTUPUS_SH $REPO_FOLDER/$OCTUPUS_SH
 }
 
 octupus_merge() {
-    cd data-airflow-dags
+    cd $REPO_FOLDER
     git config user.email "octupus@bonnierbroadcasting.com"
     git config user.name "Octupus"
-    chmod +x octupus-merge.sh
-    ./octupus-merge.sh
+    chmod +x $OCTUPUS_SH
+    ./$OCTUPUS_SH
     cd ..
 }
 
@@ -54,14 +62,18 @@ clean_up(){
     unset ADD_SECRET
     unset GIT_CLONE
     unset GIT_CHECKOUT_MERGE_SCRIPT
+    unset OCTUPUS_FOLDER
+    unset OCTUPUS_GIT
+    unset OCTUPUS_SH
+    unset REPO_FOLDER
 }
 
 #Removing rubbish upon exit
 trap clean_up EXIT
 
 #make_temp_folder
-rm -rf scripts
-rm -rf data-airflow-dags
+rm -rf $OCTUPUS_FOLDER
+rm -rf $REPO_FOLDER
 setup_git
 pull_octupus
 pull_repo
